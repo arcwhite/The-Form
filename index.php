558d73c47d1ae0d87ffb3886dc48ed8cccc2ddc5
Andy White <arcwhite@arcwhite.org>
2011-04-17 17:21:58 +1000

    Oops. Changed title tag.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-	<title>An XHTML 1.0 Strict standard template</title>
+	<title>The Form - It's For Filling Out</title>
 	<meta http-equiv="content-type" 
 		content="text/html;charset=utf-8" />
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
