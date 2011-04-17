4c809853dd6fd13d7cf238c5f7b5bcc4e9c25c80
Andy White <arcwhite@arcwhite.org>
2011-04-17 15:26:46 +1000

    Adds some fancier form styling

--- index.php
+++ index.php
@@ -12,38 +12,31 @@
 	<title>An XHTML 1.0 Strict standard template</title>
 	<meta http-equiv="content-type" 
 		content="text/html;charset=utf-8" />
-		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
-		<script type="text/javascript" src="./validate.js"></script>
-	<style>
-		input {
-			clear: right;
-			display: block;
-		}
-		
-		label {
-			display: block;
-
-		}
-		
-	</style>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+	<script type="text/javascript" src="./validate.js"></script>
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen,print"/>
 </head>
 
 <body>
-	<h1>The Form</h1>
-	<form action="process.php" method="POST">
-		<label for="name">Name:</label>
-		<input class="placeholder" rel="Your Name" id="name" name="name" value="<?php echo $name; ?>"/>
-		<label for="address">Address:</label>
-		<input class="placeholder" rel="Your Address" name="address" value="<?php echo $address; ?>"/>
-		<label for="email">Email:</label>
-		<input class="placeholder" rel="user@example.com" id="email" name="email" value="<?php echo $email; ?>"/>
-
-		<input type="checkbox" name="subscribed" value="1"/>
-		<label for="subscribed">Subscribe to newsletter?</label>
+	<div class="main">
+		<h1>The Form</h1>
+		<p>Please fill out the following form. It will sign you up to our amazing newsletter, a move that you will never in a million years regret.</p>
+		<form action="process.php" method="POST">
+			<label for="name">Name:</label>
+			<input class="placeholder" rel="Your Name" id="name" name="name" value="<?php echo $name; ?>"/>
+			<label for="address">Address:</label>
+			<input class="placeholder" rel="Your Address" name="address" value="<?php echo $address; ?>"/>
+			<label for="email">Email:</label>
+			<input class="placeholder" rel="user@example.com" id="email" name="email" value="<?php echo $email; ?>"/>
 		
-		<textarea name="comments" class="placeholder" rel="Comments?"><?php echo $comments; ?></textarea>
-		
-		<button>Submit</button>
-	</form>
+			<label for="comments">Comments</label>
+			<textarea name="comments" class="placeholder" rel="Comments?"><?php echo $comments; ?></textarea>
+			
+			<label for="subscribed">Subscribe to newsletter?</label>		
+			<input class="radio" type="radio" name="subscribed" value="Yes"/>Yes<br/>
+			<input class="radio" type="radio" name="subscribed" value="No"/>No
+			<button>Submit</button>
+		</form>
+	</div>
 </body>
 </html>
