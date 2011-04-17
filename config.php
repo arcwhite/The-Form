90b477ff2038a5db500c0dae961ddce8dd5922c0
Andy White <arcwhite@arcwhite.org>
2011-04-17 14:46:32 +1000

    Moved config options into a separate file

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Various config options for this script.
+ * Most should be pretty self-explanatory, I hope.
+ *
+ */
+
+define("MYSQL_HOST", "localhost");
+define("MYSQL_DB",   "pollenizer_test");
+define("MYSQL_USER", "pollenizer_test");
+define("MYSQL_PASS", "password");
+
+define("EMAIL_FROM_ADDR", "admin@test.com");
+define("EMAIL_FROM_NAME", "Admin User");
+
+define("SMTP_SERVER", 	"mail.test.com");
+define("SMTP_USER", 	"admin");
+define("SMTP_PASSWORD", "password");
+
+?>
--- process.php
+++ process.php
@@ -4,18 +4,7 @@
  * Yeah, I could have made this code OO, but there didn't seem like a lot of point.
  * 2011 Andy White (arcwhite@arcwhite.org)
  */
-define("MYSQL_HOST", "localhost");
-define("MYSQL_DB", 	 "pollenizer_test");
-define("MYSQL_USER", "pollenizer_test");
-define("MYSQL_PASS", "password");
-
-define("EMAIL_FROM_ADDR", "admin@test.com");
-define("EMAIL_FROM_NAME", "Admin User");
-
-define("SMTP_SERVER", 	"mail.test.com");
-define("SMTP_USER", 	"admin");
-define("SMTP_PASSWORD", "password");
-
+require("config.php");
 require("rb.php");
 require("Swift-4.0.6/lib/swift_required.php");
 
